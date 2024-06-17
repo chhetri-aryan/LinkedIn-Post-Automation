763e1a739542eff415ac979a3107dfd471cc9252
Aryan Chhetri <72271864+chhetri-aryan@users.noreply.github.com>
2024-06-17 16:21:49 +0530

    Update get-urn.py

--- get-urn.py
+++ get-urn.py
@@ -4,7 +4,7 @@ import json
 url = "https://api.linkedin.com/v2/userinfo"
 
 headers = {
-    'Authorization': 'Bearer AQV5Z8lmiTsIYjmO8isWHvYwJChaIhczx6YLvLF8P6AhtO-p_aC1vjq4XWEpVm8gn4E36-9kewnbzRaP12U_P37wlVhdTlYznQ5wJ1xO4hP22MLdcJjCSK3-W3G0ntZzdUxjdiVXhsu5DJ0Kz1j-162uJpznS-bfs4eOrPmYuay4ZOKUaEwcuhuUgGwkYcbfT5RzGh5U92lYC4LtU-GiulT8eUWxpy9i9_fe-A2AWX1OJMcFG_2lCUb9CBfx20K8lqmgHlW5280Izi5hcqQZNrOOSHza5fEx7br4R0x201LF243GTm5AFsTAENijrwEo5rL1YStjspvHNYt6IOBuP_SiBvJmDg',
+    'Authorization': '1',
     'X-Restli-Protocol-Version': '2.0.0'
 }
 
